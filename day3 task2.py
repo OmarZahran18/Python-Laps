a02b0488f3953e0edc583d6c54de5a5b21d20740
OmarZahran18 <zahrano954@gmail.com>
2025-07-06 20:10:16 +0300

    Update day3 task2.py

--- day3 task2.py
+++ day3 task2.py
@@ -6,12 +6,20 @@ domines = [
     "ali@gmail.com",
     "sara@yahoo.com",
     "mohamed@outlook.com",
-    "omar@"
-    "zahran.yahoo@com"
-    "ahmed.com@gmail"
-    "sayed@outlookcom"
+    "omar@",
+    "zahran.yahoo@com",
+    "ahmed.com@gmail",
+    "sayed@outlookcom",
     "khaled hotmail@"
 ]
 
-valid_emails = filter(lambda e: '@' in e and not e.startswith('@') and not e.endswith('@') and '.' in e.split ('@')[-1] , domines)
+valid_emails = filter(
+    lambda e: 
+        "@" in e and 
+        "." in e.split("@")[-1] and 
+        e[0] != "@" and 
+        e[-1] != "@",
+    domines
+)
+
 print(list(valid_emails))
